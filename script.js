74a0cdb39b0f2d0b47e52f8b1c8f030c6281e8d7
Caro Ferreiro <carolina.ferreiro@mail.utdt.edu>
2023-06-12 18:34:42 -0300

    jellow

--- script.js
+++ script.js
@@ -8,41 +8,36 @@ d3.csv('datos.csv', d3.autoType).then(data => {
     d.mes = formatMonth(d.Fecha);
     d.dia = formatDay(d.Fecha);
   });
-
   console.log(data)
 
-    let chart = Plot.plot({
-      marks: [
-        Plot.axisY({ 
-          anchor: "Left",
-          tickSize: 0,
-          label: null, 
-          labelOffset: 45,
-          fontSize: 13,
-          fontWeight: 500,
-        }),
-        Plot.axisX({ 
-            anchor: "Bottom",
-            grid: true, 
-            label: 'Horas escuchadas por semana', 
-            labelOffset: 40,
-            fontSize: 12,
-          }),
-        Plot.barY(data, 
-          Plot.groupX({y: 'sum'}, {
-            // x: 'msPlayed', 
-            x: d => d.dia + "/" + d.mes, 
-            filter: (d) => d.Fecha = '2023-05-27',  
-            thresholds: d3.timeWeek,
-            fill: 'Fuente',
-            // strokeOpacity: 0.2,
-            // strokeWeight: 0.2,
-            legend: true,
-            sort: 'Fuente',
-            //title: (d) =>
-            //`${d.nacionalidad}`
-            //Horas de misión: ${(d.mision_hs).toFixed(2)} horas`,
-          })),
+  let chart = Plot.plot({
+    marks: [
+      Plot.axisX({ 
+        tickSize: 0,
+        label: null, 
+        labelOffset: 0,
+        fontWeight: 500,
+      }),
+      Plot.axisY({ 
+          tickSize: 4,
+          grid: true, 
+          label: 'Horas escuchadas por día', 
+          labelOffset: 70,
+      }),
+      Plot.barY(data, 
+        Plot.groupX({y: 'sum'}, {
+          x: (d) => d.dia + "-" + d.mes, 
+          // y: (d) => d.msPlayed/(60000*60),
+          thresholds: d3.timeWeek,
+          fill: 'Fuente',
+          // strokeOpacity: 0.2,
+          // strokeWeight: 0.2,
+          legend: true,
+          sort: 'Fuente',
+          //title: (d) =>
+          //`${d.nacionalidad}`
+          //Horas de misión: ${(d.mision_hs).toFixed(2)} horas`,
+        })),
         // Plot.barX(data, 
         //     Plot.groupY({x: 'sum'}, {
         //       x: (d) => d.msPlayed/(60000*60), 
@@ -68,14 +63,15 @@ d3.csv('datos.csv', d3.autoType).then(data => {
         label: null,
       },
       style: {
-        fontSize: 12,
+        fontSize: 13,
         fontFamily: 'Tahoma',
         fontWeight: 400,
       },
       marginLeft: 70,
       marginBottom: 70,
       height: 280,
-      width: 500,     
+      insetBottom: 5,
+      width: 600,     
     })
     d3.select('#chart').append(() => chart)
   })
